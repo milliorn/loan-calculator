ae6edd5f676e6362188ac111c0d003df8c3373a3
milliorn <scottmilliorn@gmai.com>
2023-07-24 16:45:43 -0700

    theme

---
+++ theme.ts
@@ -0,0 +1,14 @@
+import { createTheme } from "@mui/material";
+
+
+export const theme = createTheme({
+  palette: {
+    mode: "dark",
+    primary: {
+      main: "#f44336",
+    },
+    secondary: {
+      main: "#3f51b5",
+    },
+  },
+});
